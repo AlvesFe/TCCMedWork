27ab4af9c982bbdd93265bba9c2445e188e6bb03
NATHANPEREIRACAVALCA <nathan.rodrigu3s@gmail.com>
2020-09-20 16:06:07 -0300

    Alteração Banco de dados

---
+++ MedWork API/routes/farmacia.js
@@ -0,0 +1,136 @@
+/* 
+    Este é o arquivo de CRUD (Create, Read, Update, Delete)
+    ou no caso PGPD (Post, Get, Patch, Delete) da entidade Hospital do projeto MEDWORK,
+    Toda manipulação de dados da farmacia feitas pelo APP ou Site do projeto 
+    passarão por aqui para efetuar alterações no banco de dados.
+*/
+
+//Importação do Express JS
+const express = require('express');
+
+//Uso do método Router do Express para escolher a função desejada
+const router = express.Router();
+
+//Importação do Banco de dados MySql
+const mysql = require('../mysql').pool;
+
+//CREATE (POST) - Recebe o valor externo e envia o pedido de inserção de dados do banco de dados
+router.post('/', (req, res, next) => {
+
+    mysql.getConnection((error, conn) => {
+
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'INSERT INTO tbl_Farmacia(nome, telefone, endereco, detalhes, cnpj, senha, email, fk_id_MedWork)VALUES(?,?,?,?,?,?,?,?)',
+            [req.body.nome, req.body.telefone, req.body.endereco, req.body.detalhes, req.body.cnpj, req.body.senha, req.body.email, req.body.fk_id_MedWork],
+            (error, resultado, field) => {
+                conn.release()
+
+                if (error) { return res.status(500).send({ error: error }) }
+
+                res.status(201).send({
+                    mensagem: 'Hospital Cadastrado',
+                    id_Farmacia: resultado.insertId
+                })
+            }
+        )
+    })
+})
+
+//READ (GET) - Busca e exibe todos os valores existentes da tabela do banco de dados
+router.get('/', (req, res, next) => {
+
+    mysql.getConnection((error, conn) => {
+
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM tbl_Hospital',
+            (error, resultado, fields) => {
+                conn.release()
+
+                if (error) { return res.status(500).send({ error: error }) }
+
+                res.status(200).send({
+                    data: resultado
+                })
+            }
+        )
+    })
+})
+
+//READ ESPECIFICO - Busca e exibe um item especifico da tabela do banco de dados
+router.get('/:id_Hospital', (req, res, next) => {
+
+    mysql.getConnection((error, conn) => {
+
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM tbl_Hospital WHERE id_Hospital = ?',
+            [req.params.id_Hospital],
+            (error, resultado, fields) => {
+                conn.release()
+
+                if (error) { return res.status(500).send({ error: error }) }
+
+                res.status(200).send({
+                    data: resultado
+                })
+            }
+        )
+    })
+})
+
+//UPDATE (PATCH) - Modifica um valor existente da tabela do banco de dados 
+router.patch('/', (req, res, next) => {
+
+    mysql.getConnection((error, conn) => {
+
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            `UPDATE tbl_Hospital
+                SET
+                   nome = ?,
+                   endereco = ?,
+                   telefone = ?,
+                   ativo = ?,
+                   foto = ?,
+                   senha = ?
+                WHERE id_Hospital = ?`,
+            [req.body.nome, req.body.endereco, req.body.telefone, req.body.ativo, req.body.foto, req.body.senha, req.body.id_Hospital],
+            (error, resultado, field) => {
+                conn.release()
+
+                if (error) { return res.status(500).send({ error: error }) }
+
+                res.status(202).send({
+                    mensagem: 'Hospital Atualizado',
+                    response: resultado.insertId
+                })
+            }
+        )
+    })
+})
+
+//DELETE - Apaga um valor existente da tabela do banco de dados
+router.delete('/', (req, res, next) => {
+
+    mysql.getConnection((error, conn) => {
+
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            `DELETE FROM tbl_Hospital WHERE id_Hospital = ?`,
+            [req.body.id_Hospital],
+            (error, resultado, field) => {
+                conn.release()
+
+                if (error) { return res.status(500).send({ error: error }) }
+
+                res.status(202).send({
+                    mensagem: 'Hospital excluído com sucesso'
+                })
+            }
+        )
+    })
+})
+
+module.exports = router;
--- MedWork Database/MedWork_Tables.sql
+++ MedWork Database/MedWork_Tables.sql
@@ -143,7 +143,7 @@ CREATE TABLE IF NOT EXISTS tbl_Recepcionista(
     email VARCHAR(90) NOT NULL UNIQUE,
     celular VARCHAR(20) NOT NULL,
     telefone VARCHAR(20) NOT NULL,
-    fk_id_MedWork INT UNSIGNED NOT NULL
+    fk_id_Hospital INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tbl_MedWork(
@@ -174,7 +174,7 @@ ADD
 ALTER TABLE
     tbl_Recepcionista
 ADD
-    CONSTRAINT fk_id_MedWork_Rec FOREIGN KEY(fk_id_MedWork) REFERENCES tbl_MedWork(id_MedWork);
+    CONSTRAINT fk_id_Hospital_Rec FOREIGN KEY(fk_id_Hospital) REFERENCES tbl_Hospital(id_Hospital);
 
 #Farmacia
 ALTER TABLE
@@ -1047,57 +1047,3 @@ VALUES
     );
 
 -- -----------------------------------------------------------------------------------------------------------------------------------------------
-#SELCETS QUE SERAM USADOS
-#SELECT LOGIN PACIENTE
-SELECT
-    *
-FROM
-    tbl_Paciente
-WHERE
-    cpf = '?'
-    AND senha = '?';
-
-#SELECT RECEITAS DO PACIENTE
-SELECT
-    tblR.*,
-    tblRR.*,
-    tblM.id_Medico,
-    tblM.nome,
-    tblM.crm,
-    tblRM.id_Remedio,
-    tblRM.nome,
-    tblRM.preco,
-    tblRM.tarja
-FROM
-    tbl_Receita As tblR
-    CROSS JOIN tbl_Receita_Remedio As tblRR
-    CROSS JOIN tbl_Medico As tblM
-    CROSS JOIN tbl_Remedio As tblRM
-WHERE
-    tblR.fk_id_Paciente = '?'
-    AND tblRR.fk_id_Receita = tblR.id_Receita
-    AND tblR.fk_id_Medico = tblM.id_Medico
-    AND tblRR.fk_id_Remedio = tblRM.id_Remedio;
-
-#SELECT BUSCAR REMDIOS
-SELECT
-    tblRF.*,
-    tblF.id_Farmacia,
-    tblF.nome,
-    tblF.telefone,
-    tblF.endereco,
-    tblF.detalhes,
-    tblF.cnpj,
-    tblR.id_Remedio,
-    tblR.tarja,
-    tblR.nome,
-    tblR.descricao,
-    tblR.preco
-FROM
-    tbl_Remedio_Farmacia As tblRF
-    CROSS JOIN tbl_Farmacia As tblF
-    CROSS JOIN tbl_Remedio As tblR
-WHERE
-    id_Remedio = '?'
-    AND tblRF.fk_id_Remedio = tblR.id_Remedio
-    AND tblF.id_Farmacia = tblRF.fk_id_Farmacia;
