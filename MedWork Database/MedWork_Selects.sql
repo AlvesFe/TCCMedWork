8c6219ce17a9995be3b9c77252f6f449b2670172
NATHANPEREIRACAVALCA <nathan.rodrigu3s@gmail.com>
2020-09-28 12:39:03 -0300

    Alteração das SELECTS

--- MedWork Database/MedWork_Selects.sql
+++ MedWork Database/MedWork_Selects.sql
@@ -8,21 +8,17 @@ SELECT tblR.*, tblRR.*,
 tblM.id_Medico, tblM.nome, tblM.crm, 
 tblRM.id_Remedio, tblRM.nome, tblRM.preco, tblRM.tarja
 FROM tbl_Receita As tblR 
-CROSS JOIN tbl_Receita_Remedio As tblRR 
-CROSS JOIN tbl_Medico As tblM
-CROSS JOIN tbl_Remedio As tblRM
-WHERE tblR.fk_id_Paciente = '?' 
-AND tblRR.fk_id_Receita = tblR.id_Receita 
-AND tblR.fk_id_Medico = tblM.id_Medico
-AND tblRR.fk_id_Remedio = tblRM.id_Remedio;
+LEFT JOIN tbl_Receita_Remedio As tblRR ON tblRR.fk_id_Receita = tblR.id_Receita
+LEFT JOIN tbl_Medico As tblM ON tblR.fk_id_Medico = tblM.id_Medico
+LEFT JOIN tbl_Remedio As tblRM ON tblRR.fk_id_Remedio = tblRM.id_Remedio
+WHERE tblR.fk_id_Paciente = 1;
 
 #SELECT BUSCAR REMDIOS
 SELECT tblRF.*, 
 tblF.id_Farmacia, tblF.nome, tblF.telefone, tblF.endereco, tblF.detalhes, tblF.cnpj,
 tblR.id_Remedio, tblR.tarja, tblR.nome, tblR.descricao, tblR.preco
-FROM tbl_Remedio_Farmacia As tblRF
-CROSS JOIN tbl_Farmacia As tblF
-CROSS JOIN tbl_Remedio As tblR 
-WHERE id_Remedio = '?'
-AND tblRF.fk_id_Remedio = tblR.id_Remedio
-AND tblF.id_Farmacia = tblRF.fk_id_Farmacia;
+FROM tbl_Remedio_Farmacia As tblRF 
+LEFT JOIN tbl_Farmacia As tblF ON tblF.id_Farmacia = tblRF.fk_id_Farmacia
+LEFT JOIN tbl_Remedio As tblR ON tblRF.fk_id_Remedio = tblR.id_Remedio
+WHERE id_Remedio = 1;
+
