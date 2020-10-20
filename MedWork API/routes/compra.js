d0c2cee205e3cc36d667315d72c963086e122f52
NATHANPEREIRACAVALCA <nathan.rodrigu3s@gmail.com>
2020-10-20 11:59:24 -0300

    Validation Compra adicionada

--- MedWork API/routes/compra.js
+++ MedWork API/routes/compra.js
@@ -11,121 +11,21 @@ const express = require('express');
 //Uso do método Router do Express para escolher a função desejada
 const router = express.Router();
 
-//Importação do Banco de dados MySql
-const mysql = require('../mysql').pool;
+const compraController = require('../validations/compra-validation');
 
 //CREATE (POST) - Recebe o valor externo e envia o pedido de inserção de dados do banco de dados
-router.post('/', (req, res, next) => {
-
-    mysql.getConnection((error, conn) => {
-
-        if (error) { return res.status(500).send({ error: error }) }
-        conn.query(
-            'INSERT INTO tbl_Compra (cod_fiscal, quantidade, fk_id_Paciente, fk_id_Remedio)VALUES(?,?,?,?)',
-            [req.body.cod_fiscal, req.body.quantidade, req.body.fk_id_Paciente, req.body.fk_id_Remedio],
-            (error, resultado, field) => {
-                conn.release()
-
-                if (error) { return res.status(500).send({ error: error }) }
-
-                res.status(201).send({
-                    mensagem: 'Compra Cadastrada',
-                    id_Farmacia: resultado.insertId
-                })
-            }
-        )
-    })
-})
+router.post('/', compraController.postCompra);
 
 //READ (GET) - Busca e exibe todos os valores existentes da tabela do banco de dados
-router.get('/', (req, res, next) => {
-
-    mysql.getConnection((error, conn) => {
-
-        if (error) { return res.status(500).send({ error: error }) }
-        conn.query(
-            'SELECT * FROM tbl_Compra',
-            (error, resultado, fields) => {
-                conn.release()
-
-                if (error) { return res.status(500).send({ error: error }) }
-
-                res.status(200).send({
-                    data: resultado
-                })
-            }
-        )
-    })
-})
+router.get('/', compraController.getCompras);
 
 //READ ESPECIFICO - Busca e exibe um item especifico da tabela do banco de dados
-router.get('/:id_Compra', (req, res, next) => {
-
-    mysql.getConnection((error, conn) => {
-
-        if (error) { return res.status(500).send({ error: error }) }
-        conn.query(
-            'SELECT * FROM tbl_Compra WHERE id_Compra = ?',
-            [req.params.id_Compra],
-            (error, resultado, fields) => {
-                conn.release()
-
-                if (error) { return res.status(500).send({ error: error }) }
-
-                res.status(200).send({
-                    data: resultado
-                })
-            }
-        )
-    })
-})
+router.get('/:id_Compra', compraController.getCompra);
 
 //UPDATE (PATCH) - Modifica um valor existente da tabela do banco de dados 
-router.patch('/', (req, res, next) => {
-
-    mysql.getConnection((error, conn) => {
-
-        if (error) { return res.status(500).send({ error: error }) }
-        conn.query(
-            `UPDATE tbl_Compra
-            SET
-            quantidade = ?
-            WHERE id_Compra = ?`,
-            [req.body.quantidade, req.body.id_Compra],
-            (error, resultado, field) => {
-                conn.release()
-
-                if (error) { return res.status(500).send({ error: error }) }
-
-                res.status(202).send({
-                    mensagem: 'Compra Atualizada',
-                    response: resultado.insertId
-                })
-            }
-        )
-    })
-})
+router.patch('/', compraController.patchCompra);
 
 //DELETE - Apaga um valor existente da tabela do banco de dados
-router.delete('/', (req, res, next) => {
-
-    mysql.getConnection((error, conn) => {
-
-        if (error) { return res.status(500).send({ error: error }) }
-        conn.query(
-            `DELETE FROM tbl_Compra WHERE id_Compra = ?`,
-            [req.body.id_Compra],
-            (error, resultado, field) => {
-                conn.release()
-
-                if (error) { return res.status(500).send({ error: error }) }
-
-                res.status(202).send({
-                    mensagem: 'Compra excluída com sucesso'
-                })
-            }
-        )
-    })
-})
+router.delete('/', compraController.deleteCompra);
 
 module.exports = router;
---
+++ MedWork API/validations/compra-validation.js
@@ -0,0 +1,179 @@
+//Importação da biblioteca Bcrypt
+const bcrypt = require('bcrypt');
+
+//Importação do Banco de dados MySql
+const mysql = require('../mysql').pool;
+
+//Faz a validação e inserção no banco de dados de um novo cadastro da Compra
+exports.postCompra = (req, res, next) => {
+
+    for (let key in req.body) {
+        if (!req.body[key]) {
+            return res.status(500).send({
+                error: "erro" + key + "vazio"
+            })
+        }
+    }
+
+    if (req.body.cod_fiscal.length != 40) {
+        return res.status(500).send({
+            error: "errotamanhocodigofiscal"
+        })
+    }
+
+    mysql.getConnection((error, conn) => {
+
+        if (error) { return res.status(500).send({ error: error }) }
+
+        const id_Compra = bcrypt.hashSync(Date.now().toString(), 10);
+        conn.query(
+            'INSERT INTO tbl_Compra (id_Compra, cod_fiscal, quantidade, fk_id_Paciente, fk_id_Remedio)VALUES(?, ?,?,?,?)',
+            [id_Compra, req.body.cod_fiscal, req.body.quantidade, req.body.fk_id_Paciente, req.body.fk_id_Remedio],
+            (error, resultado, field) => {
+                conn.release()
+
+                if (error) { return res.status(500).send({ error: error }) }
+
+                res.status(201).send({
+                    mensagem: 'Compra Cadastrada',
+                    id_Compra: id_Compra
+                })
+            }
+        )
+    })
+}
+
+exports.getCompras = (req, res, next) => {
+
+    mysql.getConnection((error, conn) => {
+
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM tbl_Compra',
+            (error, resultado, fields) => {
+                conn.release()
+
+                if (error) { return res.status(500).send({ error: error }) }
+
+                res.status(200).send({
+                    data: resultado
+                })
+            }
+        )
+    })
+}
+
+exports.getCompra = (req, res, next) => {
+
+    function isNullOrWhitespace(field) {
+        return !field || !field.trim();
+    }
+
+    console.log(req.params.id_Compra);
+
+    if (isNullOrWhitespace(req.params.id_Compra)) {
+        return res.status(500).send({
+            error: "erroidcompravazio"
+        })
+    }
+
+    if (req.params.id_Compra.length != 60) {
+        return res.status(500).send({
+            error: "errotamanhoidcompra"
+        })
+    }
+
+    mysql.getConnection((error, conn) => {
+
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM tbl_Compra WHERE id_Compra = ?',
+            [req.params.id_Compra],
+            (error, resultado, fields) => {
+                conn.release()
+
+                if (error) { return res.status(500).send({ error: error }) }
+
+                res.status(200).send({
+                    data: resultado
+                })
+            }
+        )
+    })
+}
+
+exports.patchCompra = (req, res, next) => {
+
+    for (let key in req.body) {
+        if (!req.body[key]) {
+            return res.status(500).send({
+                error: "erro" + key + "vazio"
+            })
+        }
+    }
+
+    if (req.body.id_Compra.length != 60) {
+        return res.status(500).send({
+            error: "errotamanhoidcompra"
+        })
+    }
+
+    mysql.getConnection((error, conn) => {
+
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            `UPDATE tbl_Compra
+            SET
+            quantidade = ?
+            WHERE id_Compra = ?`,
+            [req.body.quantidade, req.body.id_Compra],
+            (error, resultado, field) => {
+                conn.release()
+
+                if (error) { return res.status(500).send({ error: error }) }
+
+                res.status(202).send({
+                    mensagem: 'Compra Atualizada',
+                    response: resultado.insertId
+                })
+            }
+        )
+    })
+}
+
+exports.deleteCompra = (req, res, next) => {
+
+    function isNullOrWhitespace(field) {
+        return !field || !field.trim();
+    }
+
+    if (isNullOrWhitespace(req.body.id_Compra)) {
+        return res.status(500).send({
+            error: "erroidcompravazio"
+        })
+    }
+
+    if (req.body.id_Compra.length != 60) {
+        return res.status(500).send({
+            error: "errotamanhoidcompra"
+        })
+    }
+
+    mysql.getConnection((error, conn) => {
+
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            `DELETE FROM tbl_Compra WHERE id_Compra = ?`,
+            [req.body.id_Compra],
+            (error, resultado, field) => {
+                conn.release()
+
+                if (error) { return res.status(500).send({ error: error }) }
+
+                res.status(202).send({
+                    mensagem: 'Compra excluída com sucesso'
+                })
+            }
+        )
+    })
+}
