f895c63e6369f760f167de5a3cc86d7617b1d30d
NATHANPEREIRACAVALCA <nathan.rodrigu3s@gmail.com>
2020-11-03 10:19:01 -0300

    Adição da Camada Model a tabela Hist_Remedio

--- MedWork API/controller/hist_Remedio-validation.js
+++ MedWork API/controller/hist_Remedio-validation.js
@@ -5,33 +5,11 @@
     passarão por aqui para efetuar alterações no banco de dados.
 */
 
-//Importação do Banco de dados MySql
-const mysql = require('../mysql').pool;
+
 
 //FUNÇÕES GLOBAIS
 function isNullOrWhitespace(field) {
     return !field || !field.trim();
-}
-
-exports.getRemedios = (req, res, next) => {
-
-    mysql.getConnection((error, conn) => {
-
-        if (error) { return res.status(500).send({ error: error }) }
-        conn.query(
-            'SELECT * FROM Hst_Remedio',
-            (error, resultado, fields) => {
-                conn.release()
-
-                if (error) { return res.status(500).send({ error: error }) }
-
-                res.status(200).send({
-                    data: resultado
-                })
-            }
-        )
-    })
-
 }
 
 exports.getRemedio = (req, res, next) => {
@@ -47,23 +25,5 @@ exports.getRemedio = (req, res, next) => {
             error: "erroidhistoricoremedioinvalido" 
         })
     }
-
-    mysql.getConnection((error, conn) => {
-
-        if (error) { return res.status(500).send({ error: error }) }
-        conn.query(
-            'SELECT * FROM Hst_Remedio WHERE id_Historico_Remedio = ?',
-            [req.body.id_Historico_Remedio],
-            (error, resultado, fields) => {
-                conn.release()
-
-                if (error) { return res.status(500).send({ error: error }) }
-
-                res.status(200).send({
-                    data: resultado
-                })
-            }
-        )
-    })
-
+    next();
 }
--- MedWork API/routes/hist_Remedio.js
+++ MedWork API/routes/hist_Remedio.js
@@ -4,12 +4,15 @@ const express = require('express');
 //Uso do método Router do Express para escolher a função desejada
 const router = express.Router();
 
+//Chamando a controller da tabela hist_Remedio
 const histRemedioController = require('../controller/hist_Remedio-validation');
 
+//Chamando a model da tabela hist_Remedio
+const histRemedioModel = require('../model/hist_Remedio-model');
 //READ (GET) - Busca e exibe todos os valores existentes da tabela do banco de dados
-router.get('/', histRemedioController.getRemedios);
+router.get('/', histRemedioModel.getRemedios);
 
 //READ ESPECIFICO - Busca e exibe um item especifico da tabela do banco de dados
-router.post('/get', histRemedioController.getRemedio);
+router.post('/get', histRemedioController.getRemedio, histRemedioModel.getRemedio);
 
 module.exports = router;
