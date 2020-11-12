7df526bdf4b6b91f165aa146dcd6b403fcc9ccd5
NATHANPEREIRACAVALCA <nathan.rodrigu3s@gmail.com>
2020-11-12 10:34:00 -0300

    Adição de Middleware Remedio Farmacia

---
+++ MedWork API/middleware/route_remedio_Farmacia.js
@@ -0,0 +1,107 @@
+const jwt = require('jsonwebtoken');
+
+function ValidateRoute(permission, tipo) {
+    for (let key in permission) {
+        if (permission[key] === tipo.tipo) {
+            return 'next';
+        }
+    }
+    return 'Usuário inválido';
+}
+
+function Obtertoken(value) {
+    try {
+        const token = value.headers.authorization.split(' ')[1];
+        const decode = jwt.verify(token, process.env.JWT_KEY);
+        return decode;
+
+    } catch (error) {
+        return 'Usuário não autenticado';
+    }
+}
+
+exports.postremedioFarmacia = (req, res, next) => {
+    permission = ['farmacia'];
+    decode = Obtertoken(req);
+
+    if (decode === 'Usuário não autenticado') {
+        res.status(401).send({ mensagem: decode });
+    }
+
+    result = ValidateRoute(permission, decode);
+
+    if (result === 'next') {
+        next();
+    }
+    else if (result === 'Usuário inválido') {
+        return res.status(401).send({ mensagem: result })
+    }
+}
+exports.getRemedioFarmacias = (req, res, next) => {
+    permission = ['paciente', 'farmacia'];
+    decode = Obtertoken(req);
+
+    if (decode === 'Usuário não autenticado') {
+        res.status(401).send({ mensagem: decode });
+    }
+
+    result = ValidateRoute(permission, decode);
+
+    if (result === 'next') {
+        next();
+    }
+    else if (result === 'Usuário inválido') {
+        return res.status(401).send({ mensagem: result })
+    }
+}
+exports.getRemedioFarmacia = (req, res, next) => {
+    permission = ['paciente', 'farmacia'];
+    decode = Obtertoken(req);
+
+    if (decode === 'Usuário não autenticado') {
+        res.status(401).send({ mensagem: decode });
+    }
+
+    result = ValidateRoute(permission, decode);
+
+    if (result === 'next') {
+        next();
+    }
+    else if (result === 'Usuário inválido') {
+        return res.status(401).send({ mensagem: result })
+    }
+}
+exports.patchRemedioFarmacia = (req, res, next) => {
+    permission = ['farmacia'];
+    decode = Obtertoken(req);
+
+    if (decode === 'Usuário não autenticado') {
+        res.status(401).send({ mensagem: decode });
+    }
+
+    result = ValidateRoute(permission, decode);
+
+    if (result === 'next') {
+        next();
+    }
+    else if (result === 'Usuário inválido') {
+        return res.status(401).send({ mensagem: result })
+    }
+}
+exports.deleteRemedioFarmacia = (req, res, next) => {
+    permission = ['farmacia'];
+    decode = Obtertoken(req);
+
+    if (decode === 'Usuário não autenticado') {
+        res.status(401).send({ mensagem: decode });
+    }
+
+    result = ValidateRoute(permission, decode);
+
+    if (result === 'next') {
+        next();
+    }
+    else if (result === 'Usuário inválido') {
+        return res.status(401).send({ mensagem: result })
+    }
+}
--- MedWork API/routes/remedio_Farmacia.js
+++ MedWork API/routes/remedio_Farmacia.js
@@ -10,19 +10,22 @@ const remedioFarmaciaController = require('../controller/remedio_Farmacia-valida
 //Chamando a model da tabela Remedio_Farmacia
 const remedioFarmaciaModel = require('../model/remedio_Farmacia-model');
 
+//Chamando a Middleware da tabela Remedio_Farmacia
+const remedioFarmaciaMiddleware = require('../middleware/route_remedio_Farmacia');
+
 //CREATE (POST) - Recebe o valor externo e envia o pedido de inserção de dados do banco de dados
-router.post('/', remedioFarmaciaController.postRemedioFarmacia, remedioFarmaciaModel.postRemedioFarmacia);
+router.post('/', remedioFarmaciaMiddleware.postremedioFarmacia, remedioFarmaciaController.postRemedioFarmacia, remedioFarmaciaModel.postRemedioFarmacia);
 
 //READ (GET) - Busca e exibe todos os valores existentes da tabela do banco de dados
-router.get('/', remedioFarmaciaModel.getRemediosFarmacias);
+router.get('/', remedioFarmaciaMiddleware.getRemedioFarmacias, remedioFarmaciaModel.getRemediosFarmacias);
 
 //READ ESPECIFICO - Busca e exibe um item especifico da tabela do banco de dados
-router.post('/get', remedioFarmaciaController.getRemedioFarmacia, remedioFarmaciaModel.getRemediosFarmacia);
+router.post('/get', remedioFarmaciaMiddleware.getRemedioFarmacia, remedioFarmaciaController.getRemedioFarmacia, remedioFarmaciaModel.getRemediosFarmacia);
 
 //UPDATE (PATCH) - Modifica um valor existente da tabela do banco de dados 
-router.patch('/', remedioFarmaciaController.patchRemedioFarmacia, remedioFarmaciaModel.patchRemedioFarmacia);
+router.patch('/', remedioFarmaciaMiddleware.patchRemedioFarmacia, remedioFarmaciaController.patchRemedioFarmacia, remedioFarmaciaModel.patchRemedioFarmacia);
 
 //DELETE - Apaga um valor existente da tabela do banco de dados
-router.delete('/', remedioFarmaciaController.deleteREmedioFarmacia, remedioFarmaciaModel.deleteRemedioFarmacia);
+router.delete('/', remedioFarmaciaMiddleware.deleteRemedioFarmacia, remedioFarmaciaController.deleteREmedioFarmacia, remedioFarmaciaModel.deleteRemedioFarmacia);
 
 module.exports = router;
