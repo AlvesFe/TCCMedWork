f745ab1a18068715b8e3b67647fa52ac8061bbf2
NATHANPEREIRACAVALCA <nathan.rodrigu3s@gmail.com>
2020-11-12 10:28:20 -0300

    Adição da Middleware Recepcionista

--- MedWork API/routes/recepcionista.js
+++ MedWork API/routes/recepcionista.js
@@ -10,20 +10,23 @@ const recepcionistaController = require('../controller/recepcionista-validation'
 //Chamando a model da tabela Recepcionista
 const recepcionistaModel = require('../model/recepcionista-model');
 
+//Chamando a Middleware da tabela Recepcionista
+const recepcionistaMiddleware = require('../middleware/route_recepcionista');
+
 //CREATE (POST) - Recebe o valor externo e envia o pedido de inserção de dados do banco de dados
-router.post('/', recepcionistaController.postRecepcionista, recepcionistaModel.postRecepcionista);
+router.post('/', recepcionistaMiddleware.postRecepcionista, recepcionistaController.postRecepcionista, recepcionistaModel.postRecepcionista);
 
 //READ (GET) - Busca e exibe todos os valores existentes da tabela do banco de dados
-router.get('/', recepcionistaModel.getRecepcionistas);
+router.get('/', recepcionistaMiddleware.getRecepcionistas, recepcionistaModel.getRecepcionistas);
 
 //READ ESPECIFICO - Busca e exibe um item especifico da tabela do banco de dados
-router.post('/get', recepcionistaController.getRecepcionista, recepcionistaModel.getRecepcionista);
+router.post('/get', recepcionistaMiddleware.getRecepcionista, recepcionistaController.getRecepcionista, recepcionistaModel.getRecepcionista);
 
 //UPDATE (PATCH) - Modifica um valor existente da tabela do banco de dados 
-router.patch('/', recepcionistaController.patchRecepcionista, recepcionistaModel.patchRecepcionista);
+router.patch('/', recepcionistaMiddleware.patchRecepcionista, recepcionistaController.patchRecepcionista, recepcionistaModel.patchRecepcionista);
 
 //DELETE - Apaga um valor existente da tabela do banco de dados
-router.delete('/', recepcionistaController.deleteRecepcionista, recepcionistaModel.deleteRecepcionista);
+router.delete('/', recepcionistaMiddleware.deleteRecepcionista, recepcionistaController.deleteRecepcionista, recepcionistaModel.deleteRecepcionista);
 
 //Metodo de Login
 router.post('/login', recepcionistaController.LogarRecepcionista, recepcionistaModel.logarRecepcionista);
