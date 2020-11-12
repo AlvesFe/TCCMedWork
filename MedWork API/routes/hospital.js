56f76c7d3d062021229b6eb8f0c9f1fec4c03830
NATHANPEREIRACAVALCA <nathan.rodrigu3s@gmail.com>
2020-11-12 08:43:44 -0300

    Adição da Middleware Hospital

--- MedWork API/routes/hospital.js
+++ MedWork API/routes/hospital.js
@@ -10,20 +10,23 @@ const hospitalController = require('../controller/hospital-validation');
 //Chamando a model da tabela Hospital
 const hospitalModel = require('../model/hospital-model');
 
+//Chamando a Middleware da tabela Hospital
+const hospitalMiddleware = require('../middleware/route_hospital');
+
 //CREATE (POST) - Recebe o valor externo e envia o pedido de inserção de dados do banco de dados
-router.post('/', hospitalController.postHospital, hospitalModel.postHospital);
+router.post('/', hospitalMiddleware.postHospital, hospitalController.postHospital, hospitalModel.postHospital);
 
 //READ (GET) - Busca e exibe todos os valores existentes da tabela do banco de dados
-router.get('/', hospitalModel.getHospitais);
+router.get('/', hospitalMiddleware.getHospitais, hospitalModel.getHospitais);
 
 //READ ESPECIFICO - Busca e exibe um item especifico da tabela do banco de dados
-router.post('/get', hospitalController.getHospital, hospitalModel.getHospital);
+router.post('/get', hospitalMiddleware.getHospital, hospitalController.getHospital, hospitalModel.getHospital);
 
 //UPDATE (PATCH) - Modifica um valor existente da tabela do banco de dados 
-router.patch('/', hospitalController.patchHospital, hospitalModel.patchHospital);
+router.patch('/', hospitalMiddleware.patchHospital, hospitalController.patchHospital, hospitalModel.patchHospital);
 
 //DELETE - Apaga um valor existente da tabela do banco de dados
-router.delete('/', hospitalController.deleteHospital, hospitalModel.deleteHospital);
+router.delete('/', hospitalMiddleware.deleteHospital, hospitalController.deleteHospital, hospitalModel.deleteHospital);
 
 //Metodo de Login
 router.post('/login', hospitalController.logarHospital, hospitalModel.logarHospital);
