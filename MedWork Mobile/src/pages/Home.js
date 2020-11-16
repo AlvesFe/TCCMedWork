ad7cc4a0b452a0eb92c9f1178e21078d039882f5
NATHANPEREIRACAVALCA <nathan.rodrigu3s@gmail.com>
2020-11-16 06:56:02 -0300

    Adição de Video (BETA)

--- MedWork Mobile/src/pages/Home.js
+++ MedWork Mobile/src/pages/Home.js
@@ -1,13 +1,23 @@
-import React, {useContext} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import { AuthContext } from '../routes/AuthProvider';
+import { roxo } from '../constants/colors.json';
+import Video from '../components/Video';
+import env from '../../variables';
+import { YouTubeStandaloneAndroid } from 'react-native-youtube';
 
 export default function HomePage() {
-  const { logout } = useContext(AuthContext);
-
   return (
     <View style={styles.container}>
-      <Text>A home do cara vai aqui</Text>
+      <Text style={styles.title}>Olá, Seja bem-vindo à MedWork!</Text>
+      <View>
+        <Text style={styles.subTitle}>    Seja bem-vindo ao nosso aplicativo da MedWork, nele você vai poder consultar suas receitas medicas, alterar seuu perfil, e muito mais...</Text>
+        <Text style={styles.subTitle}>Confira nosso vídeo:</Text>
+      </View>
+      <View>
+        {/* <Video/> */}
+      </View>
+      <Text style={styles.title}>MedWork - Boas Vindas</Text>
     </View>
   )
 }
@@ -15,7 +25,17 @@ export default function HomePage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+  },
+  title: {
+    marginTop: 29,
+    color: roxo,
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  subTitle: {
+    marginTop: 10,
+    marginLeft: 15,
+    textAlign: 'justify'
+  },
 })
--- MedWork Mobile/variables.js
+++ MedWork Mobile/variables.js
@@ -1,20 +1,25 @@
 import Constants from 'expo-constants';
 
-export const prodUrl = "http://192.168.15.1:3001";
+export const prodUrl = "http://192.168.15.11:3001";
 export const key = "networkSenhores"
+export const api_key = "AIzaSyAT1u72BEre__s7KGO7a6B9HE5nvznnobk"
+
 
 const ENV = {
   dev: {
-    API_URL: "http://192.168.15.1:3001",
-    JWT_KEY: "networkSenhores"
+    API_URL: "http://192.168.15.11:3001",
+    JWT_KEY: "networkSenhores",
+    API_YTB_KEY: "AIzaSyAT1u72BEre__s7KGO7a6B9HE5nvznnobk"
   },
   staging: {
     API_URL: prodUrl,
-    JWT_KEY: key
+    JWT_KEY: key,
+    API_YTB_KEY: api_key
   },
   prod: {
     API_URL: prodUrl,
-    JWT_KEY: key
+    JWT_KEY: key,
+    API_YTB_KEY:api_key
   }
 };
 
