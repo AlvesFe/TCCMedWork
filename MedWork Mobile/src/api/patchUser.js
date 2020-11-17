fd1d0b08aa7eacabd3b955851a3db1b3fbac6b3f
alvesFe <felipe.alves135@etec.sp.gov.br>
2020-11-17 18:38:00 -0300

    ajustes no patch

--- MedWork Mobile/src/api/patchUser.js
+++ MedWork Mobile/src/api/patchUser.js
@@ -35,17 +35,24 @@ export default function patchUser(user, setViewing, setUser) {
 
     const dtn = user.dt_Nascimento
     let realDtn = ""
-    let userfinal = {}
+    let userfinal = new FormData()
 
     if (dtn.length === 24) {
         realDtn = user.dt_Nascimento.slice(0, -14);
-        userfinal = {...user, dt_Nascimento: realDtn}
-    }
-    else{
-        userfinal = user
+        user = {...user, dt_Nascimento: realDtn}
     }
 
+    for (const key in user) {
+        if (key === "foto" && user[key] === "default.png") {
+            userfinal.append("foto", null)
+        }
+        else{
+            userfinal.append(key, user[key])
+        }
+        
+    }
 
+    console.log(userfinal);
     async function getToken() {
         const token = await AsyncStorage.getItem("userToken").then(res => {
             return res
@@ -53,32 +60,32 @@ export default function patchUser(user, setViewing, setUser) {
         return token
     }
 
-    async function sendUserData() {
-        const token = await getToken();
-        Axios({
-            method: 'patch',
-            url: url+"/paciente",
-            data: userfinal,
-            headers: {
-                'Content-Type' : 'application/json',
-                'Authorization' : 'bearer '+token
-            }
-        }).then(async response => {
-            await AsyncStorage.setItem("userData", JSON.stringify(user))
-            setViewing(true)
-            toastSuccess(response.data.mensagem);
-        }).catch(async err => {
-            const item = await AsyncStorage.getItem("userData").then(res => {
-                return JSON.parse(res);
-            })
+    // async function sendUserData() {
+    //     const token = await getToken();
+    //     Axios({
+    //         method: 'patch',
+    //         url: url+"/paciente",
+    //         data: userfinal,
+    //         headers: {
+    //             'Content-Type' : 'multipart/formdata',
+    //             'Authorization' : 'bearer '+token
+    //         }
+    //     }).then(async response => {
+    //         await AsyncStorage.setItem("userData", JSON.stringify(user))
+    //         setViewing(true)
+    //         toastSuccess(response.data.mensagem);
+    //     }).catch(async err => {
+    //         const item = await AsyncStorage.getItem("userData").then(res => {
+    //             return JSON.parse(res);
+    //         })
 
-            const phrase = "Campo "+err.response.data.errormes+" vazio!"
+    //         const phrase = "Campo "+err.response.data.errormes+" vazio!"
 
-            toastFail(phrase)
-            setViewing(true)
-            setUser(item)
-        })
-    }
+    //         toastFail(phrase)
+    //         setViewing(true)
+    //         setUser(item)
+    //     })
+    // }
 
-    return sendUserData();
+    // return sendUserData();
 }
