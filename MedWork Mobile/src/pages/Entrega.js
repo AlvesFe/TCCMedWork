80c4972fbc34348c9a57baf6b6f127c5c840e2dd
NATHANPEREIRACAVALCA <nathan.rodrigu3s@gmail.com>
2020-11-19 18:58:51 -0300

    Front End Entrega

--- MedWork Mobile/src/pages/Entrega.js
+++ MedWork Mobile/src/pages/Entrega.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState, Component } from 'react';
 import { Button } from 'react-native-paper';
 import env from '../../variables';
-import { Azul, vermelho } from '../constants/colors.json';
+import { Azul, vermelho, cinza } from '../constants/colors.json';
 import { View, Text, StyleSheet, Dimensions, Image, Animated } from 'react-native';
 import getFarmaciaRemedio from '../api/getFarmaciaRemedio';
 import Loading from '../components/Loading';
@@ -12,9 +12,60 @@ const { height, width } = Dimensions.get('screen');
 export default function Entrega({ route, navigation }) {
     const { item, detalhes, Quantidade } = route.params
     return (
-        <View>
-            <Text>A tela de entrega fica aqui</Text>
-        </View>
+        <>
+            <View style={styles.container}>
+                <Image
+                    style={styles.stretch}
+                    source={{ uri: `${env.API_URL}/uploads/farmacia/Drogasil.png` }}
+                />
+                <View style={styles.container}>
+                    <Text style={styles.tarjaStyle}>ATENÇÃO!</Text>
+                    <Text style={styles.tarjaStyle}>MEDICAMENTOS TARJA PRETA</Text>
+                    <Text style={styles.tarjaStyle}>SÓ PODEM SER COMPRADOS PRESENCIALMENTE</Text>
+                </View>
+            </View>
+            <View style={styles.containerEndereco}>
+                <Text style={styles.labelText}>Endereço de Entrega:</Text>
+                <Text style={styles.info}>R. Manoel José Garcia, Nº53</Text>
+            </View>
+            <View style={styles.containerInfo}>
+                <Text style={styles.labelText}>OBS: TAXA DE ENTREGA: R$10,00</Text>
+            </View>
+            <View style={styles.containerButtonsPai}>
+                <View style={styles.containerButtons}>
+                    <Button
+                        mode='contained'
+                        title='VOLTAR'
+                        color={cinza}
+                        contentStyle={styles.editingButtonsRt}
+                        style={styles.editingButtonsView}
+                        labelStyle={styles.labelStyleRt}
+                    >RETIRAR NO LOCAL</Button>
+                </View>
+                <View style={styles.containerButtons}>
+                    <Button
+                        mode='contained'
+                        title='VOLTAR'
+                        color={vermelho}
+                        contentStyle={styles.editingButtons}
+                        style={styles.editingButtonsView}
+                        labelStyle={styles.labelStyle}
+                        onPress={() => navigation.goBack()}
+                    >VOLTAR</Button>
+                    <Button
+                        mode='contained'
+                        title='PRÓXIMO'
+                        color={Azul}
+                        contentStyle={styles.editingButtons}
+                        style={styles.editingButtonsView}
+                        labelStyle={styles.labelStyle}
+                        onPress={
+                            () => navigation.navigate('Entrega', { item, detalhes, Quantidade })
+                        }
+                    >PRÓXIMO</Button>
+                </View>
+            </View>
+        </>
     )
 }
 
@@ -23,5 +74,70 @@ const styles = StyleSheet.create({
         marginTop: 10,
         alignItems: 'center',
         justifyContent: 'flex-start',
+
+    },
+    stretch: {
+        width: width / 1.05,
+        height: height / 4.3,
+        borderRadius: 5,
+        resizeMode: 'stretch',
+    },
+    tarjaStyle: {
+        color: vermelho,
+        fontWeight: 'bold',
+        fontSize: 15,
+        marginBottom: 1,
+    },
+    containerEndereco: {
+        marginTop: 40,
+        marginLeft: 22
+    },
+    labelText: {
+        color: '#707070',
+        fontWeight: 'bold',
+        fontSize: 22
+    },
+    info: {
+        fontSize: 19,
+        color: '#949494'
+    },
+    containerInfo: {
+        marginTop: 20,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    editingButtonsView: {
+        marginHorizontal: 5,
+    },
+    editingButtons: {
+        width: width / 2.8,
+        height: height / 15,
+        borderColor: '#7d7d8f',
+        borderWidth: 1,
+    },
+    editingButtonsRt: {
+        width: width / 2.1,
+        height: height / 15,
+        borderColor: '#7d7d8f',
+        borderWidth: 1,
+    },
+    labelStyle: {
+        fontSize: 25,
+        fontWeight: "bold"
+    },
+    labelStyleRt: {
+        fontSize: 19,
+        fontWeight: "bold",
+        color: '#fff'
+    },
+    containerButtons: {
+        flexDirection: "row",
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20,
+        marginBottom: 5,
+    },
+    containerButtonsPai: {
+        marginTop: 30
     }
 })
