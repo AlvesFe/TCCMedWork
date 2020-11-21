93687bf5cb154d367342dcaa8dba255a516fe220
NATHANPEREIRACAVALCA <nathan.rodrigu3s@gmail.com>
2020-11-21 14:27:23 -0300

    Adição de Expo-Picker

--- MedWork Mobile/src/pages/Perfil.js
+++ MedWork Mobile/src/pages/Perfil.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect} from 'react';
-import { View, Text, StyleSheet, Dimensions, ScrollView, SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Dimensions, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import { Appbar, Avatar, Button } from 'react-native-paper';
 import { roxo, verde } from '../constants/colors.json';
-import Loading from '../components/Loading'; 
+import Loading from '../components/Loading';
 import FormInput from '../components/FormInput';
 import FormButton from '../components/FormButton';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -12,15 +13,47 @@ import env from '../../variables';
 
 const url = env.API_URL;
 
-const { height ,width } = Dimensions.get('screen')
+const { height, width } = Dimensions.get('screen')
 
 export default function PerfilPage() {
-  useStatusBar("light-content", roxo )
+  useStatusBar("light-content", roxo)
 
-  const [user,setUser] = useState({});
-  const [carregando,setCarregando] = useState(true);
-  const [viewing,setViewing] = useState(true);
-  
+  const [image, setImage] = useState(null);
+  const [user, setUser] = useState({});
+  const [carregando, setCarregando] = useState(true);
+  const [viewing, setViewing] = useState(true);
+
+  function upload(data) {
+    console.log(data);
+  }
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);  
+    setUser({...user, foto: result.uri})
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   async function getUserData() {
     try {
       const item = await AsyncStorage.getItem("userData").then(res => {
@@ -35,17 +68,13 @@ export default function PerfilPage() {
 
   useEffect(() => {
     getUserData();
-  },[])
-
-  // useEffect(() => {
-  //   console.log(user);
-  // },[user])
+  }, [])
 
   return (
     <>
       <Appbar.Header
         theme={{
-          colors:{primary: roxo}
+          colors: { primary: roxo }
         }}
       >
         <Appbar.Content title="Meu perfil" subtitle={viewing ? "Visualizar" : "Alterar"} />
@@ -56,81 +85,83 @@ export default function PerfilPage() {
       {
         !carregando &&
         <SafeAreaView style={styles.container}>
-        <ScrollView showsVerticalScrollIndicator={false}>
+          <ScrollView showsVerticalScrollIndicator={false}>
             <View style={styles.container}>
-              <Avatar.Image size={height/8} source={{uri : env.API_URL + '/uploads/paciente/default.png'}} />
+              <TouchableOpacity  onPress={pickImage}>
+                <Avatar.Image size={height / 8} source={{ uri: image != null ? image.uri :env.API_URL + '/uploads/paciente/default.png' }} />
+              </TouchableOpacity>
               <Text style={styles.name}>{user.nome}</Text>
             </View>
             <View style={styles.infoContainer}>
               <FormInput
                 labelName='Nome'
                 value={user.nome}
-                onChangeText={text => setUser({...user, nome:text})}
-                theme={{colors:{primary: verde}}}
+                onChangeText={text => setUser({ ...user, nome: text })}
+                theme={{ colors: { primary: verde } }}
                 underlineColor={verde}
                 disabled={viewing}
               />
               <FormInput
                 labelName='Nacimento'
                 value={user.dt_Nascimento.slice(0, -14)}
-                onChangeText={text => setUser({...user, dt_Nascimento:text})}
-                theme={{colors:{primary: verde}}}
+                onChangeText={text => setUser({ ...user, dt_Nascimento: text })}
+                theme={{ colors: { primary: verde } }}
                 underlineColor={verde}
                 disabled={true}
               />
               <FormInput
                 labelName='Tipo sanguineo'
                 value={user.tp_sanguineo}
-                onChangeText={text => setUser({...user, tp_sanguineo:text})}
-                theme={{colors:{primary: verde}}}
+                onChangeText={text => setUser({ ...user, tp_sanguineo: text })}
+                theme={{ colors: { primary: verde } }}
                 underlineColor={verde}
                 disabled={viewing}
               />
               <FormInput
                 labelName='Endereço'
                 value={user.endereco}
-                onChangeText={text => setUser({...user, endereco:text})}
-                theme={{colors:{primary: verde}}}
+                onChangeText={text => setUser({ ...user, endereco: text })}
+                theme={{ colors: { primary: verde } }}
                 underlineColor={verde}
                 disabled={viewing}
               />
               <FormInput
                 labelName='CPF'
                 value={user.cpf}
-                onChangeText={text => setUser({...user, cpf:text})}
-                theme={{colors:{primary: verde}}}
+                onChangeText={text => setUser({ ...user, cpf: text })}
+                theme={{ colors: { primary: verde } }}
                 underlineColor={verde}
                 disabled={true}
               />
               <FormInput
                 labelName='RG'
                 value={user.rg}
-                onChangeText={text => setUser({...user, rg:text})}
-                theme={{colors:{primary: verde}}}
+                onChangeText={text => setUser({ ...user, rg: text })}
+                theme={{ colors: { primary: verde } }}
                 underlineColor={verde}
                 disabled={true}
               />
               <FormInput
                 labelName='Telefone'
                 value={user.telefone}
-                onChangeText={text => setUser({...user, telefone:text})}
-                theme={{colors:{primary: verde}}}
+                onChangeText={text => setUser({ ...user, telefone: text })}
+                theme={{ colors: { primary: verde } }}
                 underlineColor={verde}
                 disabled={viewing}
               />
               <FormInput
                 labelName='E-mail'
                 value={user.email}
-                onChangeText={text => setUser({...user, email:text})}
-                theme={{colors:{primary: verde}}}
+                onChangeText={text => setUser({ ...user, email: text })}
+                theme={{ colors: { primary: verde } }}
                 underlineColor={verde}
                 disabled={true}
               />
               <FormInput
                 labelName='Alergias'
                 value={user.alergia}
-                onChangeText={text => setUser({...user, alergia:text})}
-                theme={{colors:{primary: verde}}}
+                onChangeText={text => setUser({ ...user, alergia: text })}
+                theme={{ colors: { primary: verde } }}
                 underlineColor={verde}
                 disabled={viewing}
               />
@@ -139,7 +170,7 @@ export default function PerfilPage() {
                 <FormButton
                   title='Alterar dados'
                   modeValue='contained'
-                  labelStyle={styles.alterButton} 
+                  labelStyle={styles.alterButton}
                   color={roxo}
                   onPress={() => {
                     setViewing(false);
@@ -161,18 +192,18 @@ export default function PerfilPage() {
                     >
                       Salvar
                     </Button>
-                  </View> 
-                    <Button
-                      mode='contained'
-                      color={roxo}
-                      contentStyle={styles.editingButtons}
-                      onPress={() => {
-                        setViewing(true)
-                        getUserData()
-                      }}
-                      style={styles.editingButtonsView}
-                    >
-                      Cancelar
+                  </View>
+                  <Button
+                    mode='contained'
+                    color={roxo}
+                    contentStyle={styles.editingButtons}
+                    onPress={() => {
+                      setViewing(true)
+                      getUserData()
+                    }}
+                    style={styles.editingButtonsView}
+                  >
+                    Cancelar
                     </Button>
                 </View>
               }
@@ -199,7 +230,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FFF',
-    marginVertical: height/30
+    marginVertical: height / 30
   },
   name: {
     fontSize: 20
@@ -210,12 +241,12 @@ const styles = StyleSheet.create({
   editingButtonsView: {
     marginHorizontal: 5
   },
-  editingButtons:{
-    width: width/3,
-    height: height/15
+  editingButtons: {
+    width: width / 3,
+    height: height / 15
   },
   alterContainer: {
     flexDirection: 'row',
-    marginTop:10
+    marginTop: 10
   }
 })
