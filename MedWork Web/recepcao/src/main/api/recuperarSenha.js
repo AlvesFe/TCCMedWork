21342c9d73c4e0e4e593f9ac02583af59c4a35af
NATHANPEREIRACAVALCA <nathan.rodrigu3s@gmail.com>
2020-11-24 20:22:37 -0300

    Adição de Recuperar senha à Recepcionista BETA

---
+++ MedWork Web/recepcao/src/main/api/recuperarSenha.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Axios from 'axios'
+import variables from "./variables";
+
+const env = variables()
+const {API_URL} = env
+
+
+export default function recuperarSenha (dados) {
+
+    dados = {
+        email: dados.email
+    }
+
+    Axios ({
+        method:'post',
+        url:API_URL+"/recepcionista/recuperarsenha",
+        data: dados,
+        headers:{
+            'Access-Control-Allow-Origin' : '*',
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        const {data} = response;
+        console.log(data);
+    }).catch(err => {
+        console.log(err);
+    })
+}
