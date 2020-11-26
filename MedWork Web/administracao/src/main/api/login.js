01a5405ca2bda486ae147ff3b5075e9241db4093
Felipe Batista <felipebatista@pop-os.localdomain>
2020-11-25 22:36:35 -0300

    pequeno ajuste nas alerts

--- MedWork Web/administracao/src/main/api/login.js
+++ MedWork Web/administracao/src/main/api/login.js
@@ -10,13 +10,8 @@ const Toast = Swal.mixin({
     toast: true,
     position: 'top',
     showConfirmButton: false,
-    timer: 2000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-    
+    timer: 1500,
+    timerProgressBar: true    
 }) 
 
 export default function doLogin (dados) {
