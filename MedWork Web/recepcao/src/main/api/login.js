2e9a1d776a5a33ae145f61b7a8ee27de1c1a649b
Nathan985 <nathan.rodrigu3s@gmail.com>
2020-11-26 06:58:44 -0300

    Sweetalert2 adicionado à Recepcionista

--- MedWork Web/recepcao/src/main/api/login.js
+++ MedWork Web/recepcao/src/main/api/login.js
@@ -1,31 +1,48 @@
 import React from 'react'
 import Axios from 'axios'
 import variables from "./variables";
+import Swal from 'sweetalert2';
 
 const env = variables()
-const {API_URL} = env
+const { API_URL } = env
 
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top',
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true
+})
 
-export default function doLogin (dados) {
+export default function doLogin(dados) {
 
     const data = {
         email: dados.identificacao,
         senha: dados.senha
     }
 
-    Axios ({
-        method:'post',
-        url:API_URL+"/recepcionista/login",
+    Axios({
+        method: 'post',
+        url: API_URL + "/recepcionista/login",
         data,
-        headers:{
-            'Access-Control-Allow-Origin' : '*',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
             'Content-Type': 'application/json'
         }
     }).then(response => {
-        const {data} = response;
+        const { data } = response;
         localStorage.setItem('current_user', data.token)
-        window.location.reload();
+        Toast.fire({
+            icon: 'success',
+            title: 'Logado com sucesso'
+        }).then(() => {
+            window.location.reload();
+        })
     }).catch(err => {
         console.log(err);
+        Toast.fire({
+            icon: 'error',
+            title: 'Falha no login'
+        })
     })
 }
