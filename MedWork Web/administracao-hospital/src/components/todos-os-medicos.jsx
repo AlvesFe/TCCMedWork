bf04a07fb52dfc73ee3b2f047958310d79ae2b00
felipe.alves135@etec.sp.gov.br <felipebatista@pop-os.localdomain>
2020-11-28 22:17:52 -0300

    transformando a pagina todos os medicos em uma classe

--- MedWork Web/administracao-hospital/src/components/todos-os-medicos.jsx
+++ MedWork Web/administracao-hospital/src/components/todos-os-medicos.jsx
@@ -1,29 +1,33 @@
-import React, {useEffect, useState} from 'react'
+import React, { Component} from 'react'
 import getAllMedicos from '../main/api/getAllMedicos';
 import Menu from './template/menu'
 
-export default props => {
-    const [medicos, setMedcios] = useState([]);
-    useEffect(() => {
+class TodosOsMedicos extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            medicos: []
+        }
         getAllMedicos().then(res => {
-            setMedcios(res);
+            console.log(res);
+            this.setState({medicos: res.data})
         });
-    }, [])
-
-    useEffect(() => {
-        medicos ? console.log(medicos): null
-    },[medicos])
-    return (
-        <div className='row bg-white'>
-            <Menu />
-            <div className='container col-md-8 col-lg-9 pt-4 animate__animated animate__fadeIn animate__fast'>
-                <h2 className='text-center font-weight-light'>TODOS OS MÉDICOS</h2>
-                <div>
-                    {/* {console.log(medicos)} */}
+    }
+    render() {
+        return (
+            <div className='row bg-white'>
+                <Menu />
+                <div className='container col-md-8 col-lg-9 pt-4 animate__animated animate__fadeIn animate__fast'>
+                    <h2 className='text-center font-weight-light'>TODOS OS MÉDICOS</h2>
+                    <div>
+                        {/* {console.log(medicos)} */}
+                    </div>
+    
                 </div>
-
             </div>
-        </div>
-    );
+        );
+    }
 }
 
+export default TodosOsMedicos;
