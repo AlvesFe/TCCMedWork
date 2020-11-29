8a4c8a0f2ab711bdba7b746dc39d0e0cc3df67e1
LemosLeonardo <leonardo_lemos@outlook.com.br>
2020-11-28 23:29:19 -0300

    Implementação da tela "Alterar recepcionista"

--- MedWork Web/administracao-hospital/src/components/alterar-funcionarios/alterar-recepcionista.jsx
+++ MedWork Web/administracao-hospital/src/components/alterar-funcionarios/alterar-recepcionista.jsx
@@ -1,45 +1,33 @@
 import React, { Component } from 'react';
 import UploadImagem from '../template/upload-imagem'
 import InputMask from 'react-input-mask';
-// import patchHospital from '../../main/api/alterarHospital'
-// import getInformacoes from '../../main/api/getInformacoes';
+// import getInformacoes from '../../main/api/getInformacoes'
+// import cadastrarFarmacia from '../../main/api/cadastrarFarmacia';
 import Menu from '../template/menu'
 import Image from '../../images/default-Upload.png';
-import AlterarSucesso from '../template/AlterarSucesso'
-// import AlterarErro from '../template/AlterarErro'
+import AlteracaoSucesso from '../template/AlterarSucesso'
+import AlteracaoErro from '../template/AlterarErro'
+// import alterarFarmacia from '../../main/api/alterarFarmacia';
 
 export default class AlterarRecepcionista extends Component {
     constructor() {
-        // let cnpj = localStorage.getItem('estabelecimento')
-        // getInformacoes(cnpj, 'hospital').then(res => {
-        //     this.setState({
-        //         id_Hospital: res.id_Hospital,
-        //         nomeEmpresa: res.nome,
-        //         image: {},
-        //         foto: res.foto,
-        //         cnpj: res.cnpj,
-        //         endereco: res.endereco,
-        //         telefone: res.telefone,
-        //         email: res.email,
-        //         ativo: res.ativo,
-        //         senha: res.senha
-        //     })
-        //     console.log(res);
-        // })
         super()
         this.state = {
-            nomeEmpresa: "",
+            nomeRecepcionista: "",
             image: {},
-            cnpj: "",
+            dataNascimento: "",
+            tipoSanguineo: "",
             endereco: "",
-            telefone: "",
-            taxa: "",
-            detalhes: "",
+            cpf: "",
+            rg: "",
             email: "",
-            senhaProvisoria: "",
+            celular: "",
+            telefone: "",
+            statusPerfil: "",
             alteracaoSucesso: "d-none",
             alteracaoErro: "d-none"
         }
+
         this.onChange = (e) => {
             const state = Object.assign({}, this.state)
             const campo = e.target.name
@@ -48,69 +36,93 @@ export default class AlterarRecepcionista extends Component {
         }
         this.onSubmit = (e) => {
             e.preventDefault()
-            console.log('click')
-            patchHospital(this.state).then(res => {
-                if (res) {
-                    this.setState({
-                        alteracaoSucesso: "col-12 animate__animated animate__fadeIn animate__fast"
-                    })
-                } else {
-                    this.setState({
-                        alteracaoErro: "col-12 animate__animated animate__fadeIn animate__fast"
-                    })
-                }  
-            })
+            console.log(this.state)
+            if (false) {
+                this.setState({
+                    alteracaoSucesso: "col-12 animate__animated animate__fadeIn animate__fast"
+                })
+            } else {
+                this.setState({
+                    alteracaoErro: "col-12 animate__animated animate__fadeIn animate__fast"
+                })
+            }
         }
     }
-
 
     render() {
         return (
             <div className='row bg-white'>
                 <Menu />
-                <div className='container col-md-8 pt-4 col-lg-9 animate__animated animate__fadeIn animate__fast '>
+                <div className='container col-md-8 col-lg-9 pt-4 animate__animated animate__fadeIn animate__fast'>
                     <h2 className='text-center font-weight-light'>ALTERAR RECEPCIONISTA</h2>
-                    <div className='row justify-content-center py-3'>
-                        <div className="col-10 py-2 form-row">
+                    <div className='row justify-content-center py-2'>
+                        <div className="col-11 form-row">
                             <div className={this.state.alteracaoSucesso}>
-                                <AlterarSucesso />
+                                <AlteracaoSucesso />
                             </div>
                             <div className={this.state.alteracaoErro}>
-                                <AlterarErro />
+                                <AlteracaoErro />
                             </div>
-                            <div className='col-12 mb-1'>
-                                <UploadImagem src={this.state.image.name ? URL.createObjectURL(this.state.image) : `http://localhost:3001/uploads/recepcionista/${this.state.foto}`} onChange={(event) => {
+                            <div className='col-12'>
+                                <UploadImagem src={this.state.image.name ? URL.createObjectURL(this.state.image) : `http://localhost:3001/uploads/medico/${this.state.foto}`} onChange={(event) => {
                                     this.setState({ image: event.target.files[0] });
                                 }} />
                             </div>
 
-                            <div className='form-group col-6 py-1'>
-                                <label htmlFor="pesquisarEstabelecimento" className='font-weight-bold mb-0'>Nome da empresa</label>
-                                <input type="text" className="form-control form-control-sm " id="pesquisarEstabelecimento" placeholder='Nome da empresa' name="nomeEmpresa" value={this.state.nomeEmpresa} onChange={this.onChange} />
-                            </div>
-                            <div className='form-group col-6 py-1'>
-                                <label htmlFor="cnpj" className='font-weight-bold mb-0'>CNPJ</label>
-                                <InputMask mask="99.999.999/9999-99" className="form-control form-control-sm " id="cnpj" placeholder='__.___.___/____-__' name='cnpj' value={this.state.cnpj} onChange={this.onChange} />
+                            <div className="col-12 form-row">
+                                <div className='form-group col-5 py-1'>
+                                    <label htmlFor="nomeRecepcionista" className='font-weight-bold mb-0'>Nome</label>
+                                    <input type="text" className="form-control form-control-sm" id="nomeRecepcionista" placeholder='Nome' name="nomeRecepcionista" value={this.state.nomeRecepcionista} onChange={this.onChange} />
+                                </div>
+                                <div className='form-group col-3 py-1'>
+                                    <label htmlFor="dataNascimento" className='font-weight-bold mb-0'>Data de nascimento</label>
+                                    <input type='date' className="form-control form-control-sm" id="dataNascimento" placeholder='DD/MM/AAAA' name='dataNascimento' value={this.state.dataNascimento} onChange={this.onChange} />
+                                </div>
+                                <div className='form-group col-2 py-1'>
+                                    <label htmlFor="tipoSanguineo" className='font-weight-bold mb-0'>Tipo sanguíneo</label>
+                                    <input type="text" className="form-control form-control-sm" id="tipoSanguineo" placeholder='O+' name='tipoSanguineo' value={this.state.tipoSanguineo} onChange={this.onChange} />
+                                </div>
+                                <div className='form-group col-2 py-1'>
+                                    <label htmlFor="statusPerfil" className='font-weight-bold mb-0'>Status</label>
+                                    <select className="form-control form-control-sm" name='statusPerfil' id="statusPerfil" value={this.state.statusPerfil} onChange={this.onChange}>
+                                        <option value="1">Ativo</option>
+                                        <option value="0">Inativo</option>
+                                    </select>
+                                </div>
+                                <div className='form-group col-6 py-1'>
+                                    <label htmlFor="endereco" className='font-weight-bold mb-0'>Endereço</label>
+                                    <input type="text" className="form-control form-control-sm" id="endereco" placeholder='Ex: Avenida paulista, 2222, São Paulo - SP' name='endereco' value={this.state.endereco} onChange={this.onChange} />
+                                </div>
+
+
+                                <div className='form-group col-3 py-1'>
+                                    <label htmlFor="cpf" className='font-weight-bold mb-0'>CPF</label>
+                                    <InputMask mask="999.999.999-99" className="form-control form-control-sm" id="cpf" placeholder='___.___.___-__' name='cpf' value={this.state.cpf} onChange={this.onChange} />
+                                </div>
+                                <div className='form-group col-3 py-1'>
+                                    <label htmlFor="rg" className='font-weight-bold mb-0'>RG</label>
+                                    <InputMask mask="99.999.999-9" className="form-control form-control-sm" id="rg" placeholder='__.___.___-_' name='rg' value={this.state.rg} onChange={this.onChange} />
+                                </div>
+
+                                <div className='form-group col-6 py-1'>
+                                    <label htmlFor="email" className='font-weight-bold mb-0'>E-mail</label>
+                                    <input type="text" className="form-control form-control-sm" id="email" placeholder='email@medwork.com' name='email' value={this.state.email} onChange={this.onChange} />
+                                </div>
+                                <div className='form-group col-3 py-1'>
+                                    <label htmlFor="celular" className='font-weight-bold mb-0'>Celular</label>
+                                    <InputMask mask="(99) 99999-9999" className="form-control form-control-sm" id="celular" placeholder='(__) _____-____' name='celular' value={this.state.celular} onChange={this.onChange} />
+                                </div>
+                                <div className='form-group col-3 py-1'>
+                                    <label htmlFor="telefone" className='font-weight-bold mb-0'>Telefone</label>
+                                    <InputMask mask="(99) 9999-9999" className="form-control form-control-sm" id="telefone" placeholder='(__) ____-____' name='telefone' value={this.state.telefone} onChange={this.onChange} />
+                                </div>
                             </div>
 
-                            <div className='form-group col-5 py-1'>
-                                <label htmlFor="endereco" className='font-weight-bold mb-0'>Endereço</label>
-                                <input type="text" className="form-control form-control form-control-sm " id="endereco" placeholder='Ex: Avenida paulista, 2222, São Paulo - SP' name='endereco' value={this.state.endereco} onChange={this.onChange} />
-                            </div>
-                            <div className='form-group col-4 py-1'>
-                                <label htmlFor="email" className='font-weight-bold mb-0'>E-mail</label>
-                                <input type="text" className="form-control form-control form-control-sm" id="email" placeholder='email@medwork.com' name='email' value={this.state.email} onChange={this.onChange} />
-                            </div>
-                            <div className='form-group col-3 py-1'>
-                                <label htmlFor="telefone" className='font-weight-bold mb-0'>Telefone</label>
-                                <InputMask mask="(99) 9999-9999" className="form-control form-control form-control-sm " id="telefone" placeholder='(__) ____-____' name='telefone' value={this.state.telefone} onChange={this.onChange} />
-                            </div>
-                        </div>
-                        <div className='col-12 text-center py-2'>
-                            <button className='btn-roxo' onClick={this.onSubmit} >SALVAR ALTERAÇÕES</button>
                         </div>
                     </div>
-
+                    <div className='text-center'>
+                        <button className='btn-roxo' onClick={this.onSubmit}>SALVAR ALTERAÇÕES</button>
+                    </div>
                 </div>
             </div>
         )
