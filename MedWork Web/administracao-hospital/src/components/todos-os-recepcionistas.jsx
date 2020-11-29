5391a717f9efff113004003338007d8771da69cb
Nathan985 <nathan.rodrigu3s@gmail.com>
2020-11-28 22:35:03 -0300

    Adição da Chamada da API à Todos Funcionarios

--- MedWork Web/administracao-hospital/src/components/todos-os-recepcionistas.jsx
+++ MedWork Web/administracao-hospital/src/components/todos-os-recepcionistas.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react'
+import getAllRecepcionista from '../main/api/getAllRecepcionista';
 import Menu from './template/menu'
 
-export default props => (
-    <div className='row bg-white'>
-        <Menu />
-        <div className='container col-md-8 col-lg-9 pt-4 animate__animated animate__fadeIn animate__fast'>
-            <h2 className='text-center font-weight-light'>TODOS OS RECEPCIONISTAS</h2>
-            <div>
+class TodosOsRecepcionista extends Component {
 
+    constructor() {
+        super()
+        this.state = {
+            recepcionista: []
+        }
+        getAllRecepcionista().then(res => {
+            this.setState({ recepcionista: res.data })
+        });
+    }
+    render() {
+        return (
+            <div className='row bg-white'>
+                <Menu />
+                <div className='container col-md-8 col-lg-9 pt-4 animate__animated animate__fadeIn animate__fast'>
+                    <h2 className='text-center font-weight-light'>TODOS OS RECEPCIONISTA</h2>
+                    <div>
+                        {
+                            this.state.recepcionista[0] &&
+                            console.log(this.state.recepcionista)
+                        }
+                    </div>
+                </div>
             </div>
+        );
+    }
+}
 
-        </div>
-    </div>
-)
-
+export default TodosOsRecepcionista;
