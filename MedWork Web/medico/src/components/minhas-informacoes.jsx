639441e3887be0155dae30f354d7177e11e8b6c4
LemosLeonardo <leonardo_lemos@outlook.com.br>
2020-11-30 10:02:40 -0300

    reestruturação das telas

--- MedWork Web/medico/src/components/minhas-informacoes.jsx
+++ MedWork Web/medico/src/components/minhas-informacoes.jsx
@@ -54,50 +54,50 @@ export default class MinhasInformacoes extends Component {
                             this.setState({ image: event.target.files[0] });
                         }} />
                         <div className="col-12 py-2 form-row">
-                            <div className='form-group col-6 py-1'>
+                            <div className='form-group col-6 '>
                                 <label htmlFor="nomeMedico" className='font-weight-bold mb-0'>Nome</label>
-                                <input type="text" className="form-control form-control-lg" id="nomeMedico" placeholder='Nome' name="nome" value={this.state.nome} onChange={this.onChange} />
+                                <input type="text" className="form-control form-control-sm" id="nomeMedico" placeholder='Nome' name="nome" value={this.state.nome} onChange={this.onChange} />
                             </div>
-                            <div className='form-group col-2 py-1'>
+                            <div className='form-group col-2 '>
                                 <label htmlFor="crm" className='font-weight-bold mb-0'>CRM</label>
-                                <input type="text" disabled={true} className="form-control form-control-lg" id="crm" placeholder='01.234-SP' name="crm" value={this.state.crm} onChange={this.onChange} />
+                                <input type="text" disabled={true} className="form-control form-control-sm" id="crm" placeholder='01.234-SP' name="crm" value={this.state.crm} onChange={this.onChange} />
                             </div>
-                            <div className='form-group col-2 py-1'>
+                            <div className='form-group col-2 '>
                                 <label htmlFor="dataNascimento" className='font-weight-bold mb-0'>Nascimento</label>
-                                <InputMask mask="99/99/9999" className="form-control form-control-lg" id="dataNascimento" placeholder='DD/MM/AAAA' name='dataNascimento' value={this.state.dataNascimento} onChange={this.onChange} />
+                                <InputMask mask="99/99/9999" className="form-control form-control-sm" id="dataNascimento" placeholder='DD/MM/AAAA' name='dataNascimento' value={this.state.dataNascimento} onChange={this.onChange} />
                             </div>
-                            <div className='form-group col-2 py-1'>
+                            <div className='form-group col-2 '>
                                 <label htmlFor="tipoSanguineo" className='font-weight-bold mb-0'>Tipo sanguíneo</label>
-                                <input type="text" className="form-control form-control-lg" id="tipoSanguineo" placeholder='O+' name='tipoSanguineo' value={this.state.tipoSanguineo} onChange={this.onChange} />
+                                <input type="text" className="form-control form-control-sm" id="tipoSanguineo" placeholder='O+' name='tipoSanguineo' value={this.state.tipoSanguineo} onChange={this.onChange} />
                             </div>
-                            <div className='form-group col-3 py-1'>
+                            <div className='form-group col-3 '>
                                 <label htmlFor="cpf" className='font-weight-bold mb-0'>CPF</label>
-                                <InputMask mask="999.999.999-99" disabled={true} className="form-control form-control-lg" id="cpf" placeholder='xxx.xxx.xxx-xx' name='cpf' value={this.state.cpf} onChange={this.onChange} />
+                                <InputMask mask="999.999.999-99" disabled={true} className="form-control form-control-sm" id="cpf" placeholder='xxx.xxx.xxx-xx' name='cpf' value={this.state.cpf} onChange={this.onChange} />
                             </div>
-                            <div className='form-group col-3 py-1'>
+                            <div className='form-group col-3 '>
                                 <label htmlFor="especialidade" className='font-weight-bold mb-0'>Especialidade</label>
-                                <input type="text" className="form-control form-control-lg" id="especiealidade" placeholder='Cirurgião' name='especiealidade' value={this.state.especiealidade} onChange={this.onChange} />
+                                <input type="text" className="form-control form-control-sm" id="especiealidade" placeholder='Cirurgião' name='especiealidade' value={this.state.especiealidade} onChange={this.onChange} />
                             </div>
-                            <div className='form-group col-3 py-1'>
+                            <div className='form-group col-3 '>
                                 <label htmlFor="rg" className='font-weight-bold mb-0'>RG</label>
-                                <InputMask mask="99.999.999-9" disabled={true} className="form-control form-control-lg" id="rg" placeholder='xx.xxx.xxx-x' name='rg' value={this.state.rg} onChange={this.onChange} />
+                                <InputMask mask="99.999.999-9" disabled={true} className="form-control form-control-sm" id="rg" placeholder='xx.xxx.xxx-x' name='rg' value={this.state.rg} onChange={this.onChange} />
                             </div>
-                            <div className='form-group col-3 py-1'>
+                            <div className='form-group col-3 '>
                                 <label htmlFor="ativo" className='font-weight-bold mb-0'>Ativo</label>
-                                <input type="text" className="form-control form-control-lg" id="ativo" placeholder='1' name='ativo' value={this.state.ativo} onChange={this.onChange} />
+                                <input type="text" className="form-control form-control-sm" id="ativo" placeholder='1' name='ativo' value={this.state.ativo} onChange={this.onChange} />
                             </div>
 
-                            <div className='form-group col-6 py-1'>
+                            <div className='form-group col-6 '>
                                 <label htmlFor="email" className='font-weight-bold mb-0'>E-mail</label>
-                                <input type="text" disabled={true} className="form-control form-control-lg" id="email" placeholder='email@medwork.com' name='email' value={this.state.email} onChange={this.onChange} />
+                                <input type="text" disabled={true} className="form-control form-control-sm" id="email" placeholder='email@medwork.com' name='email' value={this.state.email} onChange={this.onChange} />
                             </div>
-                            <div className='form-group col-3 py-1'>
+                            <div className='form-group col-3 '>
                                 <label htmlFor="celular" className='font-weight-bold mb-0'>Celular</label>
-                                <InputMask mask="(99) 99999-9999" className="form-control form-control-lg" id="celular" placeholder='(xx) xxxxx-xxxx' name='celular' value={this.state.celular} onChange={this.onChange} />
+                                <InputMask mask="(99) 99999-9999" className="form-control form-control-sm" id="celular" placeholder='(xx) xxxxx-xxxx' name='celular' value={this.state.celular} onChange={this.onChange} />
                             </div>
-                            <div className='form-group col-3 py-1'>
+                            <div className='form-group col-3 '>
                                 <label htmlFor="telefone" className='font-weight-bold mb-0'>Telefone</label>
-                                <InputMask mask="(99) 9999-9999" className="form-control form-control-lg" id="telefone" placeholder='(xx) xxxx-xxxx' name='telefone' value={this.state.telefone} onChange={this.onChange} />
+                                <InputMask mask="(99) 9999-9999" className="form-control form-control-sm" id="telefone" placeholder='(xx) xxxx-xxxx' name='telefone' value={this.state.telefone} onChange={this.onChange} />
                             </div>
                         </div>
                         <div className='col-12 text-center py-2'>
--- MedWork Web/medico/src/components/pacientes-em-espera.jsx
+++ MedWork Web/medico/src/components/pacientes-em-espera.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Menu from './template/menu'
+import Logotipo from '../images/logotipo.png'
 
 class PacientesEmEspera extends Component {
 
@@ -24,54 +25,15 @@ class PacientesEmEspera extends Component {
         return (
             < div className='row bg-white' >
                 <Menu />
-                <div className='container col-md-8 col-lg-9 pt-4 animate__animated animate__fadeIn animate__fast'>
-                    <h2 className='text-center font-weight-light'>PACIENTES EM ESPERA</h2>
-                    <div className="text-center">
-                        <span>
-                            <i className="square full red icon"></i>EMERGÊNCIA
-                        </span>
-                        <span className='px-2'>
-                            <i className="square full yellow icon"></i>PREFERENCIAL
-                        </span>
-                        <span>
-                            <i className="square outline icon"></i>NORMAL
-                        </span>
-                    </div>
-                    <div className='row justify-content-center py-5'>
-                        <div className="col-8 py-5">
+                <div className='container col-md-8 col-lg-9 animate__animated animate__fadeIn animate__fast'>
+                    <h2 className='text-center pt-4 font-weight-light'>INÍCIO</h2>
 
-                            <div>
-                                <div className="ui steps">
-                                    <div className="step bg-warning">
-                                        <i className="user icon"></i>
-                                        <div className="content">
-                                            <div className="title">[Nome.paciente]</div>
-                                            <div className="description">Descrições iniciais</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="ui steps">
-                                    <div className="step bg-danger">
-                                        <i className="user icon"></i>
-                                        <div className="content">
-                                            <div className="title">[Nome.paciente]</div>
-                                            <div className="description">Descrições iniciais</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="ui steps">
-                                    <div className="step bg-light">
-                                        <i className="user icon"></i>
-                                        <div className="content">
-                                            <div className="title">[Nome.paciente]</div>
-                                            <div className="description">Descrições iniciais</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
+                    <div className='text-center'>
+                        <img src={Logotipo} width='400px' alt=""/>
                     </div>
+                    
+
+
                 </div>
             </div >
         )
