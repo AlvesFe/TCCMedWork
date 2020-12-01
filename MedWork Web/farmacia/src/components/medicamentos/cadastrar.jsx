2e2427e11af8a3bfcc9b4d2dc5dc92c3ec6de66b
Nathan985 <nathan.rodrigu3s@gmail.com>
2020-12-01 11:45:50 -0300

    Adição de Metodo

--- MedWork Web/farmacia/src/components/medicamentos/cadastrar.jsx
+++ MedWork Web/farmacia/src/components/medicamentos/cadastrar.jsx
@@ -36,7 +36,16 @@ export default class CadastrarMedicamento extends Component {
                 if (res) {
                     this.setState({
                         cadastrarSucesso: "col-12 animate__animated animate__fadeIn animate__fast",
-                        cadastrarErro: "d-none"
+                        cadastrarErro: "d-none",
+                        codigoMedicamento: "",
+                        nomeMedicamento: "",
+                        tarjaMedicamento: "",
+                        codigoMedicamento: "",
+                        fabricanteMedicamento: "",
+                        validadeMedicamento: "",
+                        quantidadeMedicamento: "",
+                        precoMedicamento: "",
+                        descricaoMedicamento: ""
                     })
                 } else {
                     this.setState({
@@ -45,7 +54,7 @@ export default class CadastrarMedicamento extends Component {
                     })
                 }
             })
-            
+
         }
 
     }
@@ -66,15 +75,15 @@ export default class CadastrarMedicamento extends Component {
                 </div>
                 <div className="form-group col-4">
                     <label htmlFor="codigoMedicamento" className='mb-0'>Código do medicamento</label>
-                    <input type="text" className="form-control" id="codigoMedicamento" name='codigoMedicamento' placeholder="Ex: 001" value={this.state.codigoMedicamento} onChange={this.onChange}  />
+                    <input type="text" className="form-control" id="codigoMedicamento" name='codigoMedicamento' placeholder="Ex: 001" value={this.state.codigoMedicamento} onChange={this.onChange} />
                 </div>
                 <div className="form-group col-4">
                     <label htmlFor="nomeMedicamento" className='mb-0'>Nome do medicamento</label>
-                    <input type="text" className="form-control" id="nomeMedicamento" name='nomeMedicamento' placeholder="Ex: Diazepan"  value={this.state.nomeMedicamento} onChange={this.onChange}/>
+                    <input type="text" className="form-control" id="nomeMedicamento" name='nomeMedicamento' placeholder="Ex: Diazepan" value={this.state.nomeMedicamento} onChange={this.onChange} />
                 </div>
                 <div className="form-group col-4">
                     <label htmlFor="tarjaMedicamento" className='mb-0'>Tarja do medicamento</label>
-                    <select className="form-control" id="tarjaMedicamento" name="tarjaMedicamento"  value={this.state.tarjaMedicamento} onChange={this.onChange}>
+                    <select className="form-control" id="tarjaMedicamento" name="tarjaMedicamento" value={this.state.tarjaMedicamento} onChange={this.onChange}>
                         <option value="">Selecione</option>
                         <option value="preta">Preta</option>
                         <option value="amarela">Amarela</option>
@@ -86,19 +95,19 @@ export default class CadastrarMedicamento extends Component {
                 </div>
                 <div className="form-group col-4">
                     <label htmlFor="validadeMedicamento" className='mb-0'>Data de validade</label>
-                    <input type="date" className="form-control" id="validadeMedicamento" name='validadeMedicamento' value={this.state.validadeMedicamento} onChange={this.onChange}  />
+                    <input type="date" className="form-control" id="validadeMedicamento" name='validadeMedicamento' value={this.state.validadeMedicamento} onChange={this.onChange} />
                 </div>
                 <div className="form-group col-2">
                     <label htmlFor="quantidadeMedicamento" className='mb-0'>Quantidade</label>
-                    <input type="text" className="form-control" id="quantidadeMedicamento" name='quantidadeMedicamento' placeholder='1' value={this.state.quantidadeMedicamento} onChange={this.onChange}  />
+                    <input type="text" className="form-control" id="quantidadeMedicamento" name='quantidadeMedicamento' placeholder='1' value={this.state.quantidadeMedicamento} onChange={this.onChange} />
                 </div>
                 <div className="form-group col-2">
                     <label htmlFor="precoMedicamento" className='mb-0'>Preço</label>
-                    <input type="text" className="form-control" id="precoMedicamento" name='precoMedicamento' placeholder='R$__.__' value={this.state.precoMedicamento} onChange={this.onChange}  />
+                    <input type="text" className="form-control" id="precoMedicamento" name='precoMedicamento' placeholder='R$__.__' value={this.state.precoMedicamento} onChange={this.onChange} />
                 </div>
                 <div className="form-group col-12">
                     <label htmlFor="descricaoMedicamento" className='mb-0'>Descrição do medicamento</label>
-                    <textarea  className="form-control" id="descricaoMedicamento" name="descricaoMedicamento" cols="30" rows="5" placeholder='Descreva para que o medicamento é usado.' value={this.state.descricaoMedicamento} onChange={this.onChange}  ></textarea>
+                    <textarea className="form-control" id="descricaoMedicamento" name="descricaoMedicamento" cols="30" rows="5" placeholder='Descreva para que o medicamento é usado.' value={this.state.descricaoMedicamento} onChange={this.onChange}  ></textarea>
                 </div>
 
                 <div className='text-center col-12'>
