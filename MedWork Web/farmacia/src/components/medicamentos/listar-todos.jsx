ebdf0ddd4583963a03f8498534f2c4183fb3cc40
LemosLeonardo <leonardo_lemos@outlook.com.br>
2020-12-01 11:17:03 -0300

    implementação da tela listar todos os medicamentos

--- MedWork Web/farmacia/src/components/medicamentos/listar-todos.jsx
+++ MedWork Web/farmacia/src/components/medicamentos/listar-todos.jsx
@@ -2,22 +2,79 @@ import React, { Component } from 'react'
 
 export default class ListarMedicamentos extends Component {
 
-    // constructor() {
-    //     super()
-    //     this.state = {
+    constructor() {
+        super()
+        this.state = {
 
-    //     }
+        }
 
-    // }
+    }
 
 
     render() {
 
         return (
-            <div>
-                <h1>
-                    LISTAR MEDICAMENTOS
-                </h1>
+            <div className='container-fluid pt-2'>
+
+                <table className="table table-hover">
+                    <thead className='bg-light'>
+                        <tr className='text-center'>
+                            <th scope="col">CÓD</th>
+                            <th scope="col">NOME</th>
+                            <th scope="col">TARJA</th>
+                            <th scope="col">PREÇO</th>
+                            <th scope="col" className='text-center'>AÇÃO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr >
+                            <th scope="row">1</th>
+                            <td>Nemezulida</td>
+                            <td>Amarela</td>
+                            <td>10.90</td>
+                            <td className='text-center'>
+                                <button className='btn btn-sm btn-primary mr-1'>
+                                    <i className="pencil alternate icon"></i>
+                                </button >
+                                <button className='btn btn-sm btn-danger' >
+                                    <i className="trash alternate outline icon"></i>
+                                </button>
+                            </td>
+                        </tr>
+                        <tr >
+                            <th scope="row">2</th>
+                            <td>Gardenal</td>
+                            <td>Preta</td>
+                            <td>8.90</td>
+                            <td className='text-center'>
+                                <button className='btn btn-sm btn-primary mr-1'>
+                                    <i className="pencil alternate icon"></i>
+                                </button >
+                                <button className='btn btn-sm btn-danger' >
+                                    <i className="trash alternate outline icon"></i>
+                                </button>
+                            </td>
+                        </tr>
+                        <tr >
+                            <th scope="row">3</th>
+                            <td>Nemezulida</td>
+                            <td>Amarela</td>
+                            <td>15.90</td>
+                            <td className='text-center'>
+                                <button className='btn btn-sm btn-primary mr-1'>
+                                    <i className="pencil alternate icon"></i>
+                                </button >
+                                <button className='btn btn-sm btn-danger' >
+                                    <i className="trash alternate outline icon icon"></i>
+                                </button>
+                            </td>
+                        </tr>
+
+
+                    </tbody>
+                </table>
+
+
             </div>
         )
     }
